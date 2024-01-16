9ad263791211c1fec013986fad8963a8d3ec63bb
yuuki2323 <yuuki.work23@gmail.com>
2024-01-17 00:13:31 +0900

    s

--- src/app/InstagramPosts.js
+++ src/app/InstagramPosts.js
@@ -7,7 +7,7 @@ const InstagramPosts = () => {
 
   useEffect(() => {
     const fetchInstagramPosts = async () => {
-      const accessToken = 'IGQWRPS3FqNEwzbWtraGt1YV9DcTZAwYjlkTmlYcGd0WG90LTl1MU0xMDNEeEV1bmJyWVh6OXBIT1dnYUY1SExvbXZAnbXdUSk1pYk9ZAZAFdHaVl1ZAWVQVE1ud05VYVp4Vk5BaE5zSlVrMTNQem96dXRqMkEySzBCcEEZD'; // トークンをセット
+      const accessToken =process.env.NEXT_PUBLIC_TOKUN; // トークンをセット
       const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token=${accessToken}`);
       const data = await response.json();
       setPosts(data.data.slice(0, 2)); // 最新の6投稿を取得
