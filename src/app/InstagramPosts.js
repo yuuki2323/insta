210e39310335eebbdd6e7802549f7beeddc85855
yuuki2323 <yuuki.work23@gmail.com>
2024-01-16 23:58:12 +0900

    q

--- src/app/InstagramPosts.js
+++ src/app/InstagramPosts.js
@@ -10,7 +10,7 @@ const InstagramPosts = () => {
       const accessToken = 'IGQWRPS3FqNEwzbWtraGt1YV9DcTZAwYjlkTmlYcGd0WG90LTl1MU0xMDNEeEV1bmJyWVh6OXBIT1dnYUY1SExvbXZAnbXdUSk1pYk9ZAZAFdHaVl1ZAWVQVE1ud05VYVp4Vk5BaE5zSlVrMTNQem96dXRqMkEySzBCcEEZD'; // トークンをセット
       const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token=${accessToken}`);
       const data = await response.json();
-      setPosts(data.data.slice(0, 6)); // 最新の6投稿を取得
+      setPosts(data.data.slice(0, 2)); // 最新の6投稿を取得
     };
 
     fetchInstagramPosts();
